b56a019db8eb22c223a925bee9bfe7638ada18e7
Vishnu Priyan S S <vishnu32510@gmail.com>
2025-04-07 17:18:14 -0500

    Demo

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -1,7 +1,8 @@
 
 abstract class AppConstants {
   static const String appTitle = "";
-  static const baseUrl = 'http://10.0.0.144:5050';
+  static const baseUrl = 'http://64.131.106.209:5050';
+  // static const baseUrl = 'http://10.0.0.144:5050';
   // static const username = 'nb23';
   static const username = 'noviya23';
 }
